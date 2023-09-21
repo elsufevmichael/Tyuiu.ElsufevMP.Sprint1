9c59410edbf9231c4132b1c32db8297efb87a121
Tyuiu.ElsufevMP@outlook.com <mihal.els.32@gmail.com>
2023-09-21 13:52:50 +0500

    Task0

---
+++ Tyuiu.ElsufevMP.Sprint1.Task0.V21.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ElsufevMP.Sprint1.Task0.V21.Lib;
+
+namespace Tyuiu.ElsufevMP.Sprint1.Task0.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(17, res);
+        }
+    }
+}
---
+++ Tyuiu.ElsufevMP.Sprint1.Task0.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ElsufevMP.Sprint1.Task0.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ElsufevMP.Sprint1.Task0.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b306be9-ff1d-4aad-ad32-66c2918b66b9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
